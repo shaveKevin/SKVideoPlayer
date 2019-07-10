1da5e082cad93de7d58967d8deb530356dacd190
shavekevin <shavekevin@gmail.com>
2019-07-10 14:58:15 +0800

    添加模拟器版本

--- SKVideoPlayer/Classes/Replace.m
+++
@@ -1,7 +0,0 @@
-//
-//  Replace.m
-//  SKVideoPlayerDemo
-//
-//  Created by shavekevin on 2019/7/9.
-//  Copyright © 2019 shavekevin. All rights reserved.
-//
---
+++ upload_Simulator.sh
@@ -0,0 +1,1 @@
+pod repo push DSTSpecs SKVideoPlayer-Simulator.podspec --sources=git@github.com:iOSDevelopShareTeam/DSTSpecs.git,https://github.com/CocoaPods/Specs.git --allow-warnings --use-libraries
