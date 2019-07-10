bdc842125f369031db5a1ec16364c385bcced5c9
shavekevin <shavekevin@gmail.com>
2019-07-10 14:48:52 +0800

    url地址更新

--- SKVideoPlayerDemo/SKVideoPlayerDemo/ViewController.m
+++ SKVideoPlayerDemo/SKVideoPlayerDemo/ViewController.m
@@ -12,6 +12,7 @@
 @interface ViewController ()
 /** 播放属性 */
 @property (nonatomic, strong) id<IJKMediaPlayback>     player;
+
 @end
 
 @implementation ViewController
@@ -28,17 +29,33 @@
     [options setFormatOptionIntValue:1 forKey:@"reconnect"];
     [options setFormatOptionIntValue:30 *1000 * 1000 forKey: @"timeout"];
     
+    NSArray *array = @[@"http://txmov2.a.yximgs.com/upic/2019/07/09/19/BMjAxOTA3MDkxOTAyMzRfMTM3MTIxNDU4M18xNDk4MDA5MjYxMF8xXzM=_b_Bf1eae3107d295c501c17f916a6641113.mp4",
+                       @"http://txmov2.a.yximgs.com/upic/2019/07/08/08/BMjAxOTA3MDgwODQ1MTlfMTE1ODExNDY2MV8xNDkxOTU4ODAzN18xXzM=_b_Bb3d9ed32ff83f931bd67941b09dc6382.mp4"];
     // Do any additional setup after loading the view.
-    self.player = [[IJKFFMoviePlayerController alloc]initWithContentURL:[NSURL URLWithString:@"http://txmov2.a.yximgs.com/bs2/newWatermark/MTQwMDQ2NTk1NjE_zh_4.mp4"] withOptions:options];
-    [self.player setScalingMode:IJKMPMovieScalingModeAspectFill];
-    self.player.shouldAutoplay = YES;
-    self.player.view.frame = CGRectMake(0, 100, CGRectGetWidth([UIScreen mainScreen].bounds), 200);
+    self.player = [[IJKFFMoviePlayerController alloc]initWithContentURL:[NSURL URLWithString:array[1]] withOptions:options];
+    [self.player setScalingMode:IJKMPMovieScalingModeAspectFill];//缩放模式
+    self.player.shouldAutoplay = NO;// 自动播放
+    self.player.view.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    self.player.view.frame = self.view.bounds;
     [self.view addSubview:self.player.view];
-    [self.player prepareToPlay];
     self.player.view.backgroundColor = [UIColor redColor];
-    [self.player play];
-
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(play)];
+    [self.player.view addGestureRecognizer:tap];
+    
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.player prepareToPlay];
+}
+
+- (void)play {
+    
+    if ([self.player isPlaying]) {
+        [self.player stop];
+    }
+    [self.player play];
+    
+}
 
 @end
